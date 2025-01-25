8da094945cbed45ede2a8bfe39c8b5822f6329ff
Alexander Gorobets <for.gorobets@yandex.ru>
2025-01-25 15:48:36 +0300

    Боремся с тестами покрытия Jacoco

---
+++ server/src/test/java/ru/practicum/shareit/booking/BookingResponseMapperTest.java
@@ -0,0 +1,44 @@
+package ru.practicum.shareit.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.booking.dto.BookingResponseDto;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class BookingResponseMapperTest {
+
+    private Item item;
+
+    private User booker;
+
+    private Booking booking;
+
+    @BeforeEach
+    void setUp() {
+        item = new Item(1L, 1L, "Item", "Item Description", true, 1L);
+        booker = new User(1L, "Booker", "booker@example.com");
+        booking = new Booking(1L, LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1), item, booker, BookingStatus.APPROVED);
+    }
+
+    @Test
+    void toBookingResponseDto_ShouldReturnCorrectBookingResponseDto() {
+
+        BookingResponseDto bookingResponseDto = BookingResponseMapper.toBookingResponseDto(booking);
+
+        assertNotNull(bookingResponseDto);
+        assertEquals(booking.getId(), bookingResponseDto.getId());
+        assertEquals(booking.getStart(), bookingResponseDto.getStart());
+        assertEquals(booking.getEnd(), bookingResponseDto.getEnd());
+        assertEquals(item.getId(), bookingResponseDto.getItem().getId());
+        assertEquals(item.getName(), bookingResponseDto.getItem().getName());
+        assertEquals(booker.getId(), bookingResponseDto.getBooker().getId());
+        assertEquals(booking.getStatus(), bookingResponseDto.getStatus());
+    }
+}
